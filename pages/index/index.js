0196b1f699f200042b6fe2b38ee286a637b6a3ac
xinghaiyang1994 <xinghaiyang1994@sina.com>
2017-11-05 23:26:37 +0800

    kolList complete

---
+++ pages/index/index.js
@@ -0,0 +1,20 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    nav: 'notice',
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ]
+  },
+  chooseNav: function(e){
+    var nav = e.currentTarget.dataset.nav;
+    this.setData({
+      nav
+    });
+  }
+})
