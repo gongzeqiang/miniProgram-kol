29e8cbe5e40a67950d03b92d0c73080b2b73cd62
xinghaiyang1994 <xinghaiyang1994@sina.com>
2017-11-23 22:54:34 +0800

    增加制作模卡

--- pages/my/my.js
+++ pages/my/my.js
@@ -50,5 +50,19 @@ Page({
           hasUserInfo: false
         })
     }
+  },
+  isMoka: function(){
+    wx.showModal({
+      title: '提示',
+      content: '请升级为kol用户',
+      showCancel: false,
+      success: function(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
   }
 })
